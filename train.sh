8443aa9c375787e6e993184f5b55c8e78b0820d0
smartchen <2522264418@qq.com>
2026-01-06 20:26:51 +0800

    修改sh

--- train.sh
+++ train.sh
@@ -23,6 +23,12 @@ python train_Advent.py \
   -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master \
   -ent_min -cls_prior
 
+python train_AdaptSeg.py \
+  -raw \
+  -backbone resnet50 \
+  -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master \
+  -multilvl 
+
 python pretrain_RAIN.py -raw -task pretrain_RAIN \
  -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master
 
@@ -41,37 +47,13 @@ python train_MCCL.py \
   -backbone resnet50 \
   -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master
 
-CUDA_VISIBLE_DEVICES=1 python /data6/smartchen/code/SLCL/train_BCL.py \
+python train_BCL.py \
   -raw -backbone resnet50 \
   -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master \
   -train_with_s -train_with_t \
   -bs 8 -eval_bs 8
 
-# SLCL
-# in /data6/smartchen/code/SLCL/train.sh
-CUDA_VISIBLE_DEVICES=0 python /data6/smartchen/code/SLCL/train_AdaptEvery.py \
+python train_AdaptEvery.py \
   -backbone resnet50 \
   -raw -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master \
   -train_with_s -train_with_t
-
-python /data6/smartchen/code/SLCL/compute_class_centers.py \
-  -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master \
-  -raw \
-  -domain s \
-  -method hard_s \
-  -fold 0 \
-  -backbone resnet50 \
-  -batch_size 8
-
-python /data6/smartchen/code/SLCL/compute_class_centers.py \
-  -data_dir ../data/mmwhs/CT_MR_2D_Dataset_DA-master \
-  -raw \
-  -domain t \
-  -method soft_t \
-  -fold 0 \
-  -backbone resnet50 \
-  -weighted_ave \
-  -threshold -2 \
-  -low_thd 0.6 \
-  -high_thd 0.99 \
-  -batch_size 8
